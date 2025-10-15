0e40619873670599f55ad28da6f49cac71f917c7
DanyloPohrebniak <l00186153@atu.ie>
2025-10-15 12:55:46 +0100

    helloworld.py code update

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print('hello world!')
+
+tmp = input("Enter your name: ")
+
+if len(tmp) in range(4, 10):
+    if tmp.isalpha():
+        print(f'hello {tmp.title()}')
+    else:
+        print(f"{tmp.title()} isn't a name")
+else:
+    print(f"{tmp} doesn't fit requirements")
