e87ccd3f49c16fb7a186bc0cfce56bd651344ce0
Danylo Pohrebniak - STUDENT <l00186153@atu.ie>
2025-11-19 12:04:32 +0000

    unnittest fixed

--- unittest/temp.py
+++ unittest/temp.py
@@ -1,10 +1,8 @@
-
 def simple_function(input_str):
     if len(input_str) in range(4, 10):
         if input_str.isalpha():
-            print(f'hello {input_str.title()}')
+            return f'hello {input_str.title()}'
         else:
-            print(f"{input_str.title()} isn't a name")
+            return f"{input_str.title()} isn't a name"
     else:
-        print(f"{input_str} doesn't fit requirements")
-
+        return f"{input_str} doesn't fit requirements"
