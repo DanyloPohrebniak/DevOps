fb5e02cc739edb9274bdd9409aa7275ae84a9e06
Danylo Pohrebniak - STUDENT <l00186153@atu.ie>
2025-11-19 13:13:27 +0000

    Add some little fixes

--- logging/log_in.py
+++ logging/log_in.py
@@ -1,9 +1,13 @@
 import logging
 import simple_app
+
 logger = logging.getLogger(__name__)
 
 def main():
-    logging.basicConfig(filename='./DevOps/logging/myapp.log', level=logging.INFO, filemode='w')
+    logging.basicConfig(filename='./DevOps/logging/myapp.log', 
+                        level=logging.INFO, 
+                        format='%(asctime)s [%(levelname)s] %(name)s: %(lineno)d - %(message)s',
+                        filemode='w')
     logger.info('Started')
     simple_app.do_something()
     logger.info('Finished')
--- logging/simple_app.py
+++ logging/simple_app.py
@@ -3,11 +3,11 @@ logger = logging.getLogger(__name__)
 
 def do_something():
     for i in range(0, 10):
-        if i%2 == 0:
+        if i == 0:
+            logging.error(f"{i} is a zero")
+        elif i%2 == 0:
             logging.info(f"{i} is an even number")
         elif i%2 != 0:
             logging.warning(f"{i} is an odd number")
-        elif i == 0:
-            logging.error(f"{i} is a zero")
         else:
             logging.critical(f"{i} isn`t a number")
