46bd9ecae46b3d71121990e314a5b21eed4edd13
Danylo Pohrebniak - STUDENT <l00186153@atu.ie>
2025-11-19 12:38:17 +0000

    Adds a simple logging script

---
+++ logging/log_in.py
@@ -0,0 +1,12 @@
+import logging
+import simple_app
+logger = logging.getLogger(__name__)
+
+def main():
+    logging.basicConfig(filename='./DevOps/logging/myapp.log', level=logging.INFO, filemode='w')
+    logger.info('Started')
+    simple_app.do_something()
+    logger.info('Finished')
+
+if __name__ == '__main__':
+    main()
