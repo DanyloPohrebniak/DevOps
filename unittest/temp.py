639f004b00edc01ead043fe950c21ceeaea53567
DanyloPohrebniak <L00186153@atu.ie>
2025-12-01 23:04:46 +0000

    Add new unnittest file and function; Now TestClass.py is a class

--- unittest/temp.py
+++
@@ -1,7 +0,0 @@
-def simple_function(input_str):
-    if len(input_str) in range(4, 10):
-        if input_str.isalpha():
-            return f'hello {input_str.title()}'
-        else:
-            return f"{input_str.title()} isn't a name"
-    return f"{input_str} doesn't fit requirements"
---
+++ unittests/test2.py
@@ -0,0 +1,21 @@
+import unittest
+from unittests.TestClass import TestClass
+
+class TestSecondFunction(unittest.TestCase):
+    """Tests for second_function"""
+
+    def test_positive(self):
+        result = TestClass.second_function(5)
+        self.assertEqual(result, "positive")
+
+    def test_negative(self):
+        result = TestClass.second_function(-3)
+        self.assertEqual(result, "negative")
+
+    def test_zero(self):
+        result = TestClass.second_function(0)
+        self.assertEqual(result, "zero")
+
+
+if __name__ == '__main__':
+    unittest.main()
